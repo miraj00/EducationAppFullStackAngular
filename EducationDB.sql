7de19f082484f24009e6aee1ed5ef5946dda8791
ab <abdullahbilbaria@yahoo.com>
2023-04-13 21:52:02 -0400

    fill sql file with sample data'

---
+++ EducationDB.sql
@@ -0,0 +1,28 @@
+CREATE TABLE `animals` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(300) DEFAULT NULL,
+  `location` varchar(100) DEFAULT NULL,
+  `habitat` varchar(300) DEFAULT NULL,
+  `lifespan` varchar(50) DEFAULT NULL,
+  `biggest_threat` varchar(300) DEFAULT NULL,
+  `diet` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)  
+  );
+
+INSERT INTO animals (name, location, habitat, lifespan, biggest_threat,diet) VALUES
+
+('Cheetah', 'Africa', 'Open grassland', '10-12 years', 'habitat loss', 'carnivore');
+
+
+CREATE TABLE `notes` (     
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `title` varchar(300) DEFAULT NULL,
+  `body` varchar(5000) DEFAULT NULL, 
+   PRIMARY KEY (`id`)  
+);
+
+INSERT INTO notes (title, body) VALUES
+('favorite animal', 'I love Cheetahs');
+
+select * from animals;
+select * from notes;
