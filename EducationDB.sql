29c7689f1cabfe5084b9d9e380c8946bd91c883e
Aranda <ajabiro1211@gmail.com>
2023-04-20 21:12:33 -0400

    filled sql file with plant data

--- EducationDB.sql
+++ EducationDB.sql
@@ -15,7 +15,6 @@ CREATE TABLE `animals` (
   );
 
 INSERT INTO animals (name, location, habitat, lifespan, biggest_threat,diet) VALUES
-
 ('Cheetah', 'Africa', 'Open grassland', '10-12 years', 'habitat loss', 'carnivore');
 
 
@@ -25,9 +24,23 @@ CREATE TABLE `notes` (
   `body` varchar(5000) DEFAULT NULL, 
    PRIMARY KEY (`id`)  
 );
+ 
 
 INSERT INTO notes (title, body) VALUES
 ('favorite animal', 'I love Cheetahs');
 
+CREATE TABLE `plants` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `common_name` varchar(300) DEFAULT NULL,
+  `cycle` varchar(100) DEFAULT NULL,
+  `watering` varchar(300) DEFAULT NULL,
+  `sunlight` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)  
+  );
+  
+INSERT INTO plants (common_name, cycle, watering, sunlight) VALUES
+('rose cactus', 'perennial', 'average', 'full sun');
+
 select * from animals;
 select * from notes;
+select * from plants;
