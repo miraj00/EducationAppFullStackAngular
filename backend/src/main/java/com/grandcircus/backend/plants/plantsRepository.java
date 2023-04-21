59f5a72fa6520a4f6b249f60bc52bc1fbeda7288
Aranda <ajabiro1211@gmail.com>
2023-04-20 21:06:35 -0400

    plant backend created

---
+++ backend/src/main/java/com/grandcircus/backend/plants/plantsRepository.java
@@ -0,0 +1,7 @@
+package com.grandcircus.backend.plants;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface plantsRepository extends JpaRepository <Plants, Long> {
+
+}
